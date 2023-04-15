de395ee164074b0125ef025a2c72593c887b2111
Vitor545 <tutornews2@gmail.com>
2023-04-15 11:21:12 -0300

    add server

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://localhost:7208',
+	baseURL: 'https://kanbanwebsocketback-production.up.railway.app',
 });
 
 api.interceptors.request.use(async config => {
